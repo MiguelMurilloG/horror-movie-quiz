663eed02760f0cdf791e063b52df7b3d72003be7
Miguel Murillo <fernandomurillo@hotmail.co.uk>
2024-04-20 14:34:01 +0000

    Add list of instructions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,11 @@
+let instructionBox = document.getElementById("quiz-instructions");
 
+function openInstructions() {
+    document.getElementById('start-button').style.display = 'none';
+    instructionBox.classList.add("open-box");
+}
 
-document.getElementById('quiz').innerHTML = "Are you ready to play?";
+function closeInstructions() {
+    instructionBox.classList.remove("open-box");
+    document.getElementById('start-button').style.display = 'block';
+}
