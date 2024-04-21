017f38a9667d340bb85236825dbfde617dd2c18e
Miguel Murillo <fernandomurillo@hotmail.co.uk>
2024-04-21 14:51:45 +0000

    Add event listener to the questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,6 +16,13 @@ document.getElementById("instructions-btn").addEventListener("click", openInstru
 // Event listener for closing the instructions
 document.getElementById("close-instructions-btn").addEventListener("click", closeInstructions);
 
-
+//Fun
 const question = document.getElementById("question")
-const
+const option = Array.from (document.getElementsByClassName("option-text"))
+
+let currentQuestion ={};
+let correctAnswers = true;
+let score = 0;
+let questionCounter = 0;
+let availableQuestions = []
+
