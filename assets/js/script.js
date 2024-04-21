af66956fb0a92435c93bb6fd647b2252663779d1
Miguel Murillo <fernandomurillo@hotmail.co.uk>
2024-04-21 11:27:02 +0000

    Add background quiz.HTML

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,3 +15,5 @@ document.getElementById("instructions-btn").addEventListener("click", openInstru
 
 // Event listener for closing the instructions
 document.getElementById("close-instructions-btn").addEventListener("click", closeInstructions);
+
+
