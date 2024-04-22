ec67fadb5897ea52b699de0fb87897155975f33c
Miguel Murillo <fernandomurillo@hotmail.co.uk>
2024-04-22 16:20:32 +0000

    Add array quiz container

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,88 +1,70 @@
-const question = document.getElementById ("question");
-const options = document.getElementsByClassName("option-text");
+const question = document.getElementById("question");
+const options = Array.from(document.getElementsByClassName("option-text"));
 
-let currentQuestion ={};
-let correctAnswers = true;
+let currentQuestion = {};
+let correctAnswer = false;
 let score = 0;
 let questionCounter = 0;
-let availableQuestions = [];
+let remainingQuestions = [];
 
 // CONSTANTS
 const CORRECT_BONUS = 10;
 const MAX_QUESTIONS = 5;
 
-// Function of the start of the game
-
-function runGame(){
-    questionCounter = 0;
-    score =0;
-    remainingQuestions = [...questions]; 
-    displayNextQuestion();
-};
-
-function displayNextQuestion () {
-if (remainingQuestions.length === 0 || questionCounter >= MAX_QUESTIONS) {
-    //Show end message 
-    showEndMessage ();
-    return;
-}
-
-questionCounter++;
-
-
-}
- 
-
-
-questionCounter++;
-console.log
-
-const questions =[
+//Array of questions
+const questions = [
     {
-        question: "In 'The Exorcist', directed by William Friedkin, what is the name of the possesed girl?",
-        option1: "Emily",
-        option2: "Sarah",
-        option3: "Regan",
-        option4: "Linda",
-        answer: 3,
-    
-    },
-
-    {
-        question: "Which horror movie tells the story of a girl who gets a cursed videotape and is told she will die in seven days?",
-        option1: "Scream",
-        option2: "The Conjuring",
-        option3: "Halloween",
-        option4: "The Ring",
-        answer: 4,
-    },
-
-    {
-        question: "The Texas Chainsaw Massacres Leatherface wears a mask made from what?",
-        option1: "Leather",
-        option2: "Skin",
-        option3: "Plastic",
-        option4: "Wool",
+        question: "In 'The Exorcist', directed by William Friedkin, what is the name of the possessed girl?",
+        options: ["Emily", "Sarah", "Regan", "Linda"],
         answer: 2,
     },
-
     {
-        question: "What popular horror movie was the first feature of Freddy Krueger'",
-        option1: "A Nightmare on Elm Street",
-        option2: "Hellraiser",
-        option3: "Dracula",
-        option4:"Pet Sementary",
+        question: "Which horror movie tells the story of a girl who gets a cursed videotape and is told she will die in seven days?",
+        options: ["Scream", "The Conjuring", "Halloween", "The Ring"],
+        answer: 3,
+    },
+    {
+        question: "The Texas Chainsaw Massacre's Leatherface wears a mask made from what?",
+        options: ["Leather", "Skin", "Plastic", "Wool"],
         answer: 1,
     },
-
+    {
+        question: "What popular horror movie was the first feature of Freddy Krueger?",
+        options: ["A Nightmare on Elm Street", "Hellraiser", "Dracula", "Pet Sematary"],
+        answer: 0,
+    },
     {
         question: "What real-life incident is 'Poltergeist' rumored to have been inspired by?",
-        option1: "A haunted house in England",
-        option2: "The Amityville Horror case",
-        option3: "The Enfield Poltergeist",
-        option4: "The Bermuda Triangle phenomenon",
-        answer: 3,
-
+        options: ["A haunted house in England", "The Amityville Horror case", "The Enfield Poltergeist", "The Bermuda Triangle phenomenon"],
+        answer: 2,
     }
-
 ];
+
+startGame = () => {
+    questionCounter = 0;
+    score = 0;
+    remainingQuestions = [...questions];
+    console.log(remainingQuestions);
+    getNewQuestion();
+};
+
+getNewQuestion = () => {
+    if (remainingQuestions.length === 0 || questionCounter >= MAX_QUESTIONS) {
+        // End the game
+        return;
+    }
+    questionCounter++;
+    const questionIndex = Math.floor(Math.random() * remainingQuestions.length);
+    currentQuestion = remainingQuestions[questionIndex];
+    question.innerText = currentQuestion.question;
+    
+    // Set options text
+    options.forEach((option, index) => {
+        option.innerText = currentQuestion.options[index];
+    });
+    
+    // Remove the selected question from the remaining questions array
+    remainingQuestions.splice(questionIndex, 1);
+};
+
+startGame();
