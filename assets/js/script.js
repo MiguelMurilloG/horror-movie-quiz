4f19c75daee00d56836ffbc868395ad14d2efbed
Miguel Murillo <fernandomurillo@hotmail.co.uk>
2024-04-23 13:25:18 +0000

    Add recovering original last version

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,48 +16,8 @@ document.getElementById("instructions-btn").addEventListener("click", openInstru
 // Event listener for closing the instructions
 document.getElementById("close-instructions-btn").addEventListener("click", closeInstructions);
 
+//Function for the questions
+const question = document.getElementById("question")
+const option = Array.from (document.getElementsByClassName("option-text"))
 
 
-//  CONSTANTS
-
-const quizQuestions = document.getElementById('quiz-questions');
-const quizOptions =document.getElementById('quiz-options');
-const nextQuestion = document.getElementById('next-btn');
-const movieReveal = document.getElementById('movieReveal');
-
-let quizIndex = 0;
-let score = 0;
-let progress = 1;
-
-
-//Function fot the start the quiz
-
-function startGame() {
-    shuffleQuiz(askQuiz);
-    askQuiz.splice(4, askQuiz.length - 4)
-    quizIndex =0;
-    score = 0;
-    nextQuestion.innerHTML = 'Next';
-    showQuestion();
-    document.getElementById('quiz-progress').style.display ='block';
-    document.getElementById('quiz-progress').innerHTML = `Question ${numOfNum} of 5`;
-}
-
-// Questions
-
-function shuffleQuiz(array) {
-    for (let i = array.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [array[i], array[j]] = [array[j], array[i]];
-    }
-}
-
-//Function to show the questions
-
-function showQuestion (){
-    quizQuestions.innerHTML = '';
-    movieReveal.innerHTML = '';
-    replaceActual ();
-    let currentQuestion = askQuiz[quizIndex];
-    quizQuestions.innerHTML = currentQuestion.question;
-}
