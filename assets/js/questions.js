77dcd790ca29cbdd5a08bcbefbd12a13b60814f1
Miguel Murillo <fernandomurillo@hotmail.co.uk>
2024-04-23 09:40:48 +0000

    Change quiz container data

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,19 +1,3 @@
-const question = document.getElementById("question");
-const options = Array.from(document.getElementsByClassName("option-text"));
-const progressBar = document.getElementById("progress-bar");
-const userscore = document.getElementById("user-score");
-const endMessage = document.getElementById("end-message-text");
-const questionTracker = document.getElementById ("question-tracker");
-
-let currentQuestion = {};
-let correctAnswer = false;
-let score = 0;
-let questionCounter = 0;
-let remainingQuestions = [];
-
-// CONSTANTS
-const CORRECT_BONUS = 10;
-const MAX_QUESTIONS = 5;
 
 //Array of questions
 const questions = [
@@ -44,58 +28,3 @@ const questions = [
     }
 ];
 
-startQuiz = () => {
-    questionCounter = 0;
-    score = 0;
-    remainingQuestions = [...questions];
-    console.log(remainingQuestions);
-    getNewQuestion();
-};
-
-getNewQuestion = () => {
-    if (remainingQuestions.length === 0 || questionCounter >= MAX_QUESTIONS) {
-        // End the game
-        return;
-    }
-    questionCounter++;
-    const questionIndex = Math.floor(Math.random() * remainingQuestions.length);
-    currentQuestion = remainingQuestions[questionIndex];
-    question.innerText = currentQuestion.question;
-    
-    // Set options text
-    options.forEach((option, index) => {
-        option.innerText = currentQuestion.options[index];
-    });
-    
-    // Remove the selected question from the remaining questions array
-    remainingQuestions.splice(questionIndex, 1);
-
-     correctAnswer = true;
-}
-
-options.forEach(option =>{
-    option.addEventListener ("click", e =>{
-        if (!correctAnswer) return;
-
-        correctAnswer = false;
-        const selectedChoice = e.target; // Changed from selectedOption
-        const selectedAnswer = selectedChoice.dataset["number"]
-
-        // Check answers 
-        const checkAnswer = selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect';
-
-        if (checkAnswer === "correct") {
-            incrementScore(CORRECT_BONUS);
-        }
-
-        // Add class to indicate correct or incorrect answer
-        selectedChoice.classList.add(checkAnswer);
-
-        setTimeout(() => {
-            selectedChoice.classList.remove(checkAnswer);
-            getNewQuestion();
-        }, 1000);
-    });
-});
-
-startQuiz();
--- assets/js/script.js
+++ assets/js/script.js
@@ -17,7 +17,4 @@ document.getElementById("instructions-btn").addEventListener("click", openInstru
 document.getElementById("close-instructions-btn").addEventListener("click", closeInstructions);
 
 //Function for the questions
-const question = document.getElementById("question")
-const option = Array.from (document.getElementsByClassName("option-text"))
-
 
