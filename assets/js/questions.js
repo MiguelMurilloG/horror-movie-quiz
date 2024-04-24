7334ff1593a143b755ae9326c57a620fb92c0a2e
Miguel Murillo <fernandomurillo@hotmail.co.uk>
2024-04-24 02:49:54 +0000

    Add color to my questions

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -16,27 +16,27 @@ const questions = [
     {
         question: "In 'The Exorcist', directed by William Friedkin, what is the name of the possessed girl?",
         options: ["Emily", "Sarah", "Regan", "Linda"],
-        answer: 2,
+        answer: 3,
     },
     {
         question: "Which horror movie tells the story of a girl who gets a cursed videotape and is told she will die in seven days?",
         options: ["Scream", "The Conjuring", "Halloween", "The Ring"],
-        answer: 3,
+        answer: 4,
     },
     {
         question: "The Texas Chainsaw Massacre's Leatherface wears a mask made from what?",
         options: ["Leather", "Skin", "Plastic", "Wool"],
-        answer: 1,
+        answer: 2,
     },
     {
         question: "What popular horror movie was the first feature of Freddy Krueger?",
         options: ["A Nightmare on Elm Street", "Hellraiser", "Dracula", "Pet Sematary"],
-        answer: 0,
+        answer: 1,
     },
     {
         question: "What real-life incident is 'Poltergeist' rumored to have been inspired by?",
         options: ["A haunted house in England", "The Amityville Horror case", "The Enfield Poltergeist", "The Bermuda Triangle phenomenon"],
-        answer: 2,
+        answer: 3,
     }
 ];
 
@@ -81,6 +81,7 @@ function endQuiz() {
     // Display end message
     endMessage.textContent = "Quiz ended! Thank you for playing.";
     endMessage.style.display = "block";
+    playAgaingButton.style.display = "block";
 }
 
 // Event listeners for option selection
